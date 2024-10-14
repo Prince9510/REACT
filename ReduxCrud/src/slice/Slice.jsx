df2284ee56bd36151b5ae99b245da98f90fe1396
Prince9510 <131976898+Prince9510@users.noreply.github.com>
2024-10-14 13:56:41 +0530

    ReduxCrud

---
+++ ReduxCrud/src/slice/Slice.jsx
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const slice = createSlice({
+    name : "slice" , 
+    initialState : {
+        student : []
+    },
+    reducers : {
+        adddata:(state , action)=>{
+            state.student.push(action.payload)
+
+        },
+        deletedata:(state , action)=>{
+            state.student = state.student.filter((item)=>
+                item.id !== action.payload.id
+            )
+        }
+    }
+})
+
+
+export const {adddata , deletedata} = slice.actions
+export default slice.reducer
+
