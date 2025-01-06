4b1db27919efe7bd7dc8df262245ce7f40a56036
Prince9510 <131976898+Prince9510@users.noreply.github.com>
2025-01-06 13:26:41 +0530

    Delete src directory

--- src/Store/Store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartReducre from "../Slice/Slice";
-
- export const store = configureStore({
-    reducer: {
-        cart: cartReducre,
-    }
- })
